b6aa8d4534dfbecf7e040db062b46e1bcf135404
weix1233 <43843639+weix1233@users.noreply.github.com>
2018-10-04 16:32:41 +0800

    Update Insertion.py

--- Insertion.py
+++ Insertion.py
@@ -2,6 +2,7 @@ def insertion(data):
     for i in range(1,len(data)):
         j=i-1
         key = data[i]
+        #shift stuff down then insert
         while j>=0 and key<data[j]:
             data[j+1]=data[j]
             j-=1
